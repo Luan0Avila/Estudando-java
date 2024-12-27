f2ac4b2e320193abd863d984e67ef154fe8f0a2a
Luan0Avila <avila.almeida.luan@gmail.com>
2024-12-27 19:34:21 -0300

    Meu primeiro código em Java

---
+++ PRIMEIRO_CODIGO/OlaMundo.java
@@ -0,0 +1,13 @@
+package PRIMEIRO_CODIGO;
+public class OlaMundo {
+    public static void main(String[] args) {
+        
+        //1 - Primeiro programa
+        System.out.println("Meu primeiro programa em Java");
+
+        System.out.println(System.getProperty("java.version"));
+
+        //Comentário
+    }
+    
+}
---
+++ SINNTAXE_BASICA/ExemplosSIntaxe.java
@@ -0,0 +1,10 @@
+package SINNTAXE_BASICA;
+
+public class ExemplosSIntaxe {
+    
+    public static void main(String[] args) {
+        
+        //1 - Impressão de texto
+        System.out.println("Podemos inprimir este texto " + 45);
+    }
+}
