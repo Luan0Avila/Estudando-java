7863ee753c3c08fce2e26d5026ab653f57a599f1
Luan0Avila <avila.almeida.luan@gmail.com>
2024-12-27 19:41:05 -0300

    Aprendendo sobre alguns tipos primitivos

--- SINNTAXE_BASICA/ExemplosSIntaxe.java
+++ SINNTAXE_BASICA/ExemplosSIntaxe.java
@@ -6,5 +6,20 @@ public class ExemplosSIntaxe {
         
         //1 - Impressão de texto
         System.out.println("Podemos inprimir este texto " + 45);
+
+        //2 - tipos primitivos
+        //variavel = coloca dados
+        //int - integer = inteiro
+        int idade = 20;
+        int anoAtual = 2024;
+
+        System.out.println("Sua idade é " + idade + " anos.");
+        System.out.println("O ano é " + anoAtual);
+
+        //double para variaveis com virgula
+        double altura = 1.75;
+        double pi = 3.14;
+
+        System.out.println("Altura " + altura + " PI " + pi);
     }
 }
